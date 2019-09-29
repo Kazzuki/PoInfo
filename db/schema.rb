28494cd8aaaf2397b8e35066a4917d9516a23d15
KazukiMatsuda <madakazuki@ezweb.ne.jp>
2019-09-29 10:50:55 +0900

    [git] 企業記事作成の追加

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(version: 2019_09_28_064718) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
+    t.string "info"
     t.string "zip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
